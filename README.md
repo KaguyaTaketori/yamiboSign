7ff2f62492c93cf5cb9a9d3717b05e52460b4757
KaguyaTaketori <53823229+KaguyaTaketori@users.noreply.github.com>
2024-01-20 17:09:01 +0800

    fix some glitches

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# test
-an auto sign script for yamibo using selenium
+# yamiboSign
+
+yamiboSignは、テスト目的でWebアプリケーションを自動化するためにSelenium WebDriverを使用しています。設定は`config.ini`ファイルから読み取り、機密データには環境変数を使用します。
+
+## はじめに
+
+以下の手順に従って、開発とテストのためにローカルマシンでプロジェクトのコピーを稼働させる方法を説明します。
+
+### 前提条件
+
+- Python 3.6以上
+- Selenium WebDriver
+- ChromeDriver
+
+### インストール
+
+1. リポジトリをクローンします
+```bash
+git clone https://github.com/yourusername/yamiboSign.git
+```
+
+2. 依存関係をインストールします
+```bash
+pip install -r requirements.txt
+```
+
+3. 環境変数を設定します
+```bash
+export USERNAME=yourusername
+export PASSWORD=yourpassword
+```
+
+4. スクリプトを実行します
+```bash
+python main.py
+```
+
+### 設定
+`config.ini`ファイルには、スクリプトが使用するURLとCSSセレクタが含まれています。このファイルを自分のニーズに合わせて変更することができます。
+
+### 機能
+スクリプトは提供されたユーザ名とパスワードを使用してウェブサイトにログインし、サインインアクションを実行します。
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import configparser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -7,58 +8,73 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
-# define css selectors and other variables
 wait_time = 10
-home_url = 'https://bbs.yamibo.com/'
-login_url = 'https://bbs.yamibo.com/member.php?mod=logging&action=login'
-username_css_selector = "input[name='username']"
-password_css_selector = "input[name='password']"
-login_button_css_selector = "button[name='loginsubmit']"
-sign_button_css_selector = "#wp > div.ct2.cl > div.sd > div.bm.signbtn.cl > a"
 
-# set chrome options and start chrome
-service = Service(ChromeDriverManager().install())
-chrome_options = Options()
-chrome_options.add_argument('--headless')
-driver = webdriver.Chrome(options=chrome_options, service=service)
-print('Chrome started.')
-# print Chrome version
-print('Chrome version: ' + driver.capabilities['browserVersion'])
-# print Chrome driver version
-print('Chrome driver version: ' + driver.capabilities['chrome']['chromedriverVersion'].split(' ')[0])
+# Read configuration from config.ini
+config = configparser.ConfigParser()
+config.read('config.ini')
 
-# login
-# username and password are stored in GitHub secrets
-username = os.environ['USERNAME']
-password = os.environ['PASSWORD']
-driver.get(login_url)
-driver.find_element(By.CSS_SELECTOR, username_css_selector).send_keys(username)
-driver.find_element(By.CSS_SELECTOR, password_css_selector).send_keys(password)
-driver.find_element(By.CSS_SELECTOR, login_button_css_selector).click()
+url = {
+    'home_url': config.get('URL', 'home_url'),
+    'login_url': config.get('URL', 'login_url'),
+    'sign_url': config.get('URL', 'sign_url')
+}
 
+css_selector = {
+    'username_css_selector': config.get('CSS_SELECTOR', 'username_css_selector'),
+    'password_css_selector': config.get('CSS_SELECTOR', 'password_css_selector'),
+    'login_button_css_selector': config.get('CSS_SELECTOR', 'login_button_css_selector'),
+    'sign_button_css_selector': config.get('CSS_SELECTOR', 'sign_button_css_selector')
+}
 
-# wait for login success
-print('Waiting for login...')
-# wait for ten seconds
+def set_chrome_driver():
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(options=chrome_options, service=service)
+    print('Chrome options and service set.')
+    print('Chrome version: ' + driver.capabilities['browserVersion'])
+    print('Chrome driver version: ' + driver.capabilities['chrome']['chromedriverVersion'].split(' ')[0])
+    return driver
 
-try:
-    WebDriverWait(driver, wait_time).until(
-        EC.url_to_be(home_url)
-    )
-    print('Login success!')
-except Exception as e:
-    print('Login failed!')
-    print(e)
+def find_element_and_send_keys(driver, css_selector, keys):
+    try:
+        driver.find_element(By.CSS_SELECTOR, css_selector).send_keys(keys)
+    except Exception as e:
+        print(f"Failed to find element with CSS selector {css_selector}")
+        print(e)
 
+def login(driver, username, password):
+    driver.get(url['login_url'])
+    find_element_and_send_keys(driver, css_selector['username_css_selector'], username)
+    find_element_and_send_keys(driver, css_selector['password_css_selector'], password)
+    driver.find_element(By.CSS_SELECTOR, css_selector['login_button_css_selector']).click()
+    print('Waiting for login...')
+    try:
+        WebDriverWait(driver, wait_time).until(EC.url_to_be(url['home_url']))
+        print('Login success!')
+    except Exception as e:
+        print('Login failed!')
+        print(e)
+        return False
+    return True
 
-# sign
-driver.get('https://bbs.yamibo.com/plugin.php?id=zqlj_sign')
-sign_button = driver.find_element(By.CSS_SELECTOR, sign_button_css_selector)
-if sign_button.text != '今日已打卡':
-    sign_button.click()
-    print('Signed!')
-else:
-    print('Already signed!')
+def sign(driver):
+    driver.get(url['sign_url'])
+    sign_button = driver.find_element(By.CSS_SELECTOR, css_selector['sign_button_css_selector'])
+    if sign_button.text != '今日已打卡':
+        sign_button.click()
+        print('Signed!')
+    else:
+        print('Already signed!')
 
-# quit
-driver.quit()
+if __name__ == '__main__':
+    username = os.getenv('USERNAME', '')
+    password = os.getenv('PASSWORD', '')
+    if not username or not password:
+        print('Username or password not set in environment variables.')
+        exit(1)
+    driver = set_chrome_driver()
+    if login(driver, username, password):
+        sign(driver)
+    driver.quit()
