022cad0dc53888031d03222d20481fdc50ae9488
nanami <nanamilsp@gmail.com>
2024-01-20 14:48:23 +0800

    fix some glitches

--- main.py
+++ main.py
@@ -11,21 +11,35 @@ service = Service(ChromeDriverManager().install())
 chrome_options = Options()
 chrome_options.add_argument('--headless')
 driver = webdriver.Chrome(options=chrome_options, service=service)
+print('Chrome started.')
+# print Chrome version
+print('Chrome version: ' + driver.capabilities['browserVersion'])
+# print Chrome driver version
+print('Chrome driver version: ' + driver.capabilities['chrome']['chromedriverVersion'].split(' ')[0])
 
 # login
+# username and password are stored in environment variables in .env file
 username = os.getenv('USERNAME')
 password = os.getenv('PASSWORD')
+print('Username: ' + username)
+print('Password: ' + password)
 driver.get('https://bbs.yamibo.com/member.php?mod=logging&action=login')
 driver.find_element(By.CSS_SELECTOR, "input[name='username']").send_keys(username)
-driver.find_element(By.CSS_SELECTOR, "input[name='password']").send_keys('D5YPgaMCd.VA9nQ')
+driver.find_element(By.CSS_SELECTOR, "input[name='password']").send_keys(password)
 driver.find_element(By.CSS_SELECTOR, "button[name='loginsubmit']").click()
 
-# wait for login
+
+# wait for login success
 print('Waiting for login...')
-while True:
-    if driver.current_url == 'https://bbs.yamibo.com/':
-        break
-print('Login success!')
+# wait for ten seconds
+driver.implicitly_wait(10)
+# check whether login success
+if driver.current_url != 'https://bbs.yamibo.com/':
+    print('Login failed!')
+    driver.quit()
+    exit(1)
+else:
+    print('Login success!')
 
 # sign
 driver.get('https://bbs.yamibo.com/plugin.php?id=zqlj_sign')
