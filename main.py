e7bbe3bd9e8e8277e7350358cf2c68a2e092de53
KaguyaTaketori <53823229+KaguyaTaketori@users.noreply.github.com>
2024-01-20 15:23:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,36 +19,37 @@ print('Chrome driver version: ' + driver.capabilities['chrome']['chromedriverVer
 
 # login
 # username and password are stored in environment variables in .env file
-username = os.getenv('USERNAME') + '1'
+username = os.getenv('USERNAME')
 password = os.getenv('PASSWORD')
-print('Username: ' + username)
-print('Password: ' + password)
-driver.get('https://bbs.yamibo.com/member.php?mod=logging&action=login')
-driver.find_element(By.CSS_SELECTOR, "input[name='username']").send_keys(username)
-driver.find_element(By.CSS_SELECTOR, "input[name='password']").send_keys(password)
-driver.find_element(By.CSS_SELECTOR, "button[name='loginsubmit']").click()
+print(username = 'ArtemisK')
+# print('Username: ' + username)
+# print('Password: ' + password)
+# driver.get('https://bbs.yamibo.com/member.php?mod=logging&action=login')
+# driver.find_element(By.CSS_SELECTOR, "input[name='username']").send_keys(username)
+# driver.find_element(By.CSS_SELECTOR, "input[name='password']").send_keys(password)
+# driver.find_element(By.CSS_SELECTOR, "button[name='loginsubmit']").click()
 
 
-# wait for login success
-print('Waiting for login...')
-# wait for ten seconds
-driver.implicitly_wait(10)
-# check whether login success
-if driver.current_url != 'https://bbs.yamibo.com/':
-    print('Login failed!')
-    driver.quit()
-    exit(1)
-else:
-    print('Login success!')
+# # wait for login success
+# print('Waiting for login...')
+# # wait for ten seconds
+# driver.implicitly_wait(10)
+# # check whether login success
+# if driver.current_url != 'https://bbs.yamibo.com/':
+#     print('Login failed!')
+#     driver.quit()
+#     exit(1)
+# else:
+#     print('Login success!')
 
-# sign
-driver.get('https://bbs.yamibo.com/plugin.php?id=zqlj_sign')
-sign_button = driver.find_element(By.CSS_SELECTOR, "#wp > div.ct2.cl > div.sd > div.bm.signbtn.cl > a")
-if sign_button.text != '今日已打卡':
-    sign_button.click()
-    print('Signed!')
-else:
-    print('Already signed!')
+# # sign
+# driver.get('https://bbs.yamibo.com/plugin.php?id=zqlj_sign')
+# sign_button = driver.find_element(By.CSS_SELECTOR, "#wp > div.ct2.cl > div.sd > div.bm.signbtn.cl > a")
+# if sign_button.text != '今日已打卡':
+#     sign_button.click()
+#     print('Signed!')
+# else:
+#     print('Already signed!')
 
-# quit
-driver.quit()
+# # quit
+# driver.quit()
